2674494180ca7799a10232bed5f0956daae4790d
TheBounciestCastle <pablomelecho@gmail.com>
2026-05-20 09:19:49 +0200

    Cara el sol

--- DAM/src/main/java/com/mycompany/dam/DAM.java
+++ DAM/src/main/java/com/mycompany/dam/DAM.java
@@ -13,4 +13,28 @@ public class DAM {
     public static void main(String[] args) {
         System.out.println("Hello 67!");
     }
+
+    //    Cara al Sol, con la camisa nueva
+//    Que tú bordaste, en rojo, ayer
+//    Me hallará la muerte si me lleva
+//    Y no te vuelvo a ver
+//
+//    Formaré junto a mis compañeros
+//    Que hacen guardia sobre los luceros
+//    Impasible el ademán, que están
+//    Presentes en nuestro afán
+//
+//    Si te dicen que caí, me fui
+//    Al puesto que tengo allí
+//
+//    Volverán banderas victoriosas
+//    Al paso alegre de la paz
+//    Y traerán prendidas cinco rosas
+//    Las flechas de mi haz
+//
+//    Volverá a reír la primavera
+//    Que por cielo, tierra y mar se espera
+//
+//    Arriba, escuadras, a vencer
+//    Que, en España, empieza a amanecer
 }
