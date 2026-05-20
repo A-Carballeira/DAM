e61b4d7ed6dbe3a4966effd8f0351d430450a523
Victor-Blade <ricomontoya252@gmail.com>
2026-05-20 09:25:20 +0200

    Commit mensaje victor2

--- DAM/src/main/java/com/mycompany/dam/DAM.java
+++ DAM/src/main/java/com/mycompany/dam/DAM.java
@@ -12,12 +12,6 @@ public class DAM {
 
     public static void main(String[] args) {
         System.out.println("Hello 67!");
-        //* clase para cuando un servidor de minecraft /
-
-
-
-
-
-        /*Comentario de Victor (filantropo)*/
+        /*Comentario de Victor (filantropo, guapo y sexy)*/
     }
 }
